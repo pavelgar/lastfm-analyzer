0ccb130c4a1a21f4b98d7fc96de14b1dd6394352
pavelgar <pavel.garmuyev@gmail.com>
2020-01-29 17:20:12 +0200

    Start working on the first widget

--- src/components/ActivityChart.js
+++ src/components/ActivityChart.js
@@ -1,45 +1,33 @@
-import React, { Component } from "react"
+import React, { useEffect, useRef } from "react"
+import userService from "../services/user"
 import * as d3 from "d3"
 
-class ActivityChart extends Component {
-  componentDidMount() {
-    const data = [2, 4, 2, 6, 8]
-    this.drawBarChart(data)
-  }
-  drawBarChart(data) {
-    const canvasHeight = 400
-    const canvasWidth = 600
-    const scale = 20
+const ActivityChart = ({ user }) => {
+  const canvas = useRef(null)
 
-    const svgCanvas = d3
-      .select(this.refs.canvas)
-      .append("svg")
-      .attr("width", canvasWidth)
-      .attr("height", canvasHeight)
-      .style("border", "1px solid black")
+  useEffect(() => {
+    if (user) {
+      userService.getScrobbles(user.id).then(data => {
+        if (data) {
+          const dateCounts = d3
+            .nest()
+            .key(d => new Date(d.date * 1000).toISOString().split("T")[0])
+            .rollup(v => v.length)
+            .entries(data)
+            .reverse()
 
-    svgCanvas
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("width", 40)
-      .attr("height", datapoint => datapoint * scale)
-      .attr("fill", "orange")
-      .attr("x", (datapoint, iteration) => iteration * 45)
-      .attr("y", datapoint => canvasHeight - datapoint * scale)
+          console.log(dateCounts)
+          // draw(dates)
+        }
+      })
+    }
+  }, [user])
 
-    svgCanvas
-      .selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("x", (dataPoint, i) => i * 45 + 10)
-      .attr("y", (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
-      .text(dataPoint => dataPoint)
+  const draw = data => {
+    // TODO: Draw the graphics
   }
-  render() {
-    return <div ref="canvas"></div>
-  }
+
+  return <div ref={canvas} />
 }
+
 export default ActivityChart
